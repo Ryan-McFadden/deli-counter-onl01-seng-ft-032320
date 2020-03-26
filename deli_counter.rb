dab3535c698f1eec5e484780b60d12f8425d255d
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-26 01:46:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,5 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome, #{name[-1]}. You are number #{katz_deli[-1]} in line."
+  puts "Welcome, #{katz_deli[-1]}. You are number #{katz_deli[-1]} in line."
 end
