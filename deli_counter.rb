cd299177dcb5517c1c8b57820273f19cb41c2950
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-26 01:33:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,11 @@ def line(katz_deli)
   if katz_deli.count == 0 
     puts "The line is currently empty."
   else
+    pos = []
     katz_deli.each do |name|
-      katz_deli.index(name)
+      #{katz_deli.index(name)+1}
+      #{name}
+      pos << "#{katz_deli.index(name)}. #{name}"
   end
+  puts "The line is currently: #{pos.join("")}"
 end
