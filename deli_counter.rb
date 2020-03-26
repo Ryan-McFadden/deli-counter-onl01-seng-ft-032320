ea576aa87b0ad1a925d43b02965b6e893c9be2cf
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-26 01:16:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,7 @@ def line(katz_deli)
   if katz_deli.count == 0 
     puts "The line is currently empty."
   else
-    katz_deli.each do |name|
-      que += "#{name.index += 1}. #{name}"
-    end
-    puts "The current line is: #{que}"
+    katz_deli.each_with_index do |name, num|
+      katz_deli += 
   end
 end
